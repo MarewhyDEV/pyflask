5d75dea8732f86f0124f6214fc12fc7768697d10
MarewhyDEV <mehmetgokhankaramanoglu@gmail.com>
2023-09-09 19:13:55 +0300

    Python Flask Denemesi

---
+++ main2.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+yt = ["Yazi","Tura"]
+
+@app.route("/")
+def hello_world():
+    return '''<h1>Hello, World!</h1> 
+      <a href="/yazitura">Yazı Tura At</a>'''
+
+@app.route("/yazitura")
+def yazitura():
+    info = random.choice(yt)
+    return f'<h3>{info}</h3>'
+
+
+
+
+
+
+app.run(debug=True)
